2cb35823bc5924e4f288c25f7cb2946266e65827
admin <admin@DESKTOP-CJKMJLC>
2024-07-10 10:04:57 +0900

    첫번째릴리스

--- SSManager/src/co/yedam/common/AppTest.java
+++ SSManager/src/co/yedam/common/AppTest.java
@@ -10,13 +10,13 @@ import java.util.Scanner;
 import co.yedam.vo.StudentVO;
 
 public class AppTest {
-	
+
 	public static void main(String[] args) {
-		
+
 //    1) Oracle JDBC Driver 자바 라이브러리.
 //    2) Connection 객체.
 		Scanner scanner = new Scanner(System.in);
-		
+
 //		System.out.print("학생번호 입력 > ");
 //		String studentNumber = scanner.nextLine();
 //		
@@ -27,8 +27,7 @@ public class AppTest {
 //		String phoneNumber = scanner.nextLine();
 //		
 //		addStudent(studentNumber, studentName, phoneNumber); // 입력기능 메소드 호출
-		
-		
+
 //		System.out.print("수정할 학생번호 입력 > ");
 //		String modifyStudentNumber = scanner.nextLine();
 //		
@@ -39,56 +38,55 @@ public class AppTest {
 //		String modifyPhoneNumber = scanner.nextLine();
 //		
 //		modifyStudent(modifyStudentNumber, modifyStudentName, modifyPhoneNumber);
-		
+
 //		System.out.print("삭제할 학생번호 입력 > ");
 //		String removeStudentNumber = scanner.nextLine();
 //	
 //		removeStudent(removeStudentNumber);
-		
+
 		System.out.print("수정할 학생번호 입력 > ");
 		String modifyStudentNumber = scanner.nextLine();
-		
+
 		System.out.print("학생이름 입력 > ");
 		String modifyStudentName = scanner.nextLine();
-		
+
 		System.out.print("연락처 입력 > ");
 		String modifyPhoneNumber = scanner.nextLine();
-		
+
 		System.out.print("주소 입력 > ");
 		String modifyAddress = scanner.nextLine();
-		
+
 		System.out.print("생일 입력 > ");
 		String modifyBirth = scanner.nextLine();
-		
+
 		StudentVO studentVO = new StudentVO();
 		studentVO.setStudentNumber(modifyStudentNumber);
 		studentVO.setStudentName(modifyStudentName);
 		studentVO.setStudentPhone(modifyPhoneNumber);
 		studentVO.setAddress(modifyAddress);
 		studentVO.setBirthDate(modifyBirth);
-		
+
 		modifyStudent2(studentVO);
-		
+
 		search(); // 조회기능 메소드 호출
 	}
-	
-	
-  // 수정기능
+
+	// 수정기능
 	public static void modifyStudent(String modifyStudentNumber, String modifyStudentName, String modifyPhoneNumber) {
-		 Connection connection = getConnection();
-		 String sql = "UPDATE table_student ";
-		 sql += "SET student_name = '" + modifyStudentName + "'";
-		 sql += "    ,student_phone = '" + modifyPhoneNumber + "'";
-		 sql += "WHERE student_number = '" + modifyStudentNumber + "'";
+		Connection connection = getConnection();
+		String sql = "UPDATE table_student ";
+		sql += "SET student_name = '" + modifyStudentName + "'";
+		sql += "    ,student_phone = '" + modifyPhoneNumber + "'";
+		sql += "WHERE student_number = '" + modifyStudentNumber + "'";
 		try {
-		 Statement stament = connection.createStatement();
-		 int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
-		 System.out.println("처리된 건수는" + r + "건");
-		} catch(SQLException e) {
-			 e.printStackTrace();
+			Statement stament = connection.createStatement();
+			int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
+			System.out.println("처리된 건수는" + r + "건");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static void modifyStudent2(StudentVO studentVO) {
 		Connection connection = getConnection();
 		String sql = "UPDATE table_student ";
@@ -97,68 +95,86 @@ public class AppTest {
 		sql += "   ,address = nvl('" + studentVO.getAddress() + "', address)";
 		sql += "   ,birth_date = nvl('" + studentVO.getBirthDate() + "', birth_date)";
 		sql += "WHERE student_number = '" + studentVO.getStudentNumber() + "'";
-		
+
 		try {
-			 Statement stament = connection.createStatement();
-			 int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
-			 System.out.println("처리된 건수는" + r + "건");
-			} catch(SQLException e) {
-				 e.printStackTrace();
-			}
-	}
-	
-//	입력기능
-//	INSERT INTO table_student (student_number, student_name, student_phone)
-//	VALUES('s2024-01', '홍길동', '010-1264-5678');
-	
-	public static void addStudent(String studentNumber, String studentName, String phoneNumber) {
-		Connection connection = getConnection();
-		String sql = "INSERT INTO table_student (student_number, student_name, student_phone, address)";
-		sql += "VALUES('"+ studentNumber +"', '" + studentName + "', '" + phoneNumber + "', '서울 100번지')";
-		try {
-		Statement stament = connection.createStatement();
-		int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
-		System.out.println("처리된 건수는" + r + "건");
-		} catch(SQLException e) {
+			Statement stament = connection.createStatement();
+			int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
+			System.out.println("처리된 건수는" + r + "건");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+//	입력기능
+//	INSERT INTO table_student (student_number, student_name, student_phone)
+//	VALUES('s2024-01', '홍길동', '010-1264-5678');
+
+	public static void addStudent(String studentNumber, String studentName, String phoneNumber) {
+		Connection connection = getConnection();
+		String sql = "INSERT INTO table_student (student_number, student_name, student_phone, address)";
+		sql += "VALUES('" + studentNumber + "', '" + studentName + "', '" + phoneNumber + "', '서울 100번지')";
+		try {
+			Statement stament = connection.createStatement();
+			int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
+			System.out.println("처리된 건수는" + r + "건");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	// 삭제기능
 	public static void removeStudent(String removeStudentNumber) {
 		Connection connection = getConnection();
 		String sql = "DELETE FROM table_student ";
-    sql += "WHERE student_number = '" + removeStudentNumber + "'";
+		sql += "WHERE student_number = '" + removeStudentNumber + "'";
 		try {
-		Statement stament = connection.createStatement();
-		int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
-		System.out.println("처리된 건수는" + r + "건");
-		} catch(SQLException e) {
+			Statement stament = connection.createStatement();
+			int r = stament.executeUpdate(sql); // insert, update, delete => 처리된 건수
+			System.out.println("처리된 건수는" + r + "건");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	// 조회기능
 	public static void search() {
-		
+
 		try {
 			Connection connection = getConnection();
 			Statement stament = connection.createStatement();
 			ResultSet rs = stament.executeQuery("SELECT * FROM table_student");
 			// [객체1, 객체2, 객체3]
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String birth = rs.getString("birth_date");
 				birth = birth != null ? birth.substring(0, 10) : "";
-				System.out.println(rs.getString("student_number") + ", " + rs.getString("student_name") + ", " + rs.getString("student_phone"));
+				System.out.println(rs.getString("student_number") + ", " + rs.getString("student_name") + ", "
+						+ rs.getString("student_phone"));
 			}
-				System.out.println("end of data.");
-				
-		} catch(SQLException e) {
-				e.printStackTrace();
+			System.out.println("end of data.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
+
+	}
+
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+
+		String user = "jsp";
+		String password = "jsp";
+
+		Connection connection = null;
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("오라클 드라이버 없음");
+			e.printStackTrace();
+		}
+
+		return connection;
 	}
 }
